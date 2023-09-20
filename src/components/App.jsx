b2d9f4a0a57da45f9f02335a5a97c60711188521
Michael Sprague <mikesprague@gmail.com>
2023-09-19 20:55:13 -0400

    refactor: misc improvements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React from 'react';
+import { FaGithub, FaQuoteLeft } from 'react-icons/fa';
 import useSWR from 'swr';
-import { FaQuoteLeft, FaGithub } from 'react-icons/fa';
 import './App.scss';
 
 export default function App() {
-  const fetcher = async (url) => await axios.get(url).then((response) => response.data);
+  const fetcher = async (url) =>
+    await axios.get(url).then((response) => response.data);
+
   const { data, error } = useSWR('/api/sample-endpoint', fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
@@ -14,36 +16,41 @@ export default function App() {
 
   return (
     <>
-      <div className="container max-w-screen-xl min-h-screen p-8 text-center page-wrapper">
-        <h1 className="mb-8 text-4xl font-bold text-center underline">
-          My React/Tailwind CSS Starter for a Vercel Hosted Project
-          <br />
-        </h1>
-        <h2 className="mb-12 text-2xl font-semibold leading-tight text-center">
-          Example data below fetched from API powered by serverless function
-          <br />
-          <small className="text-base font-normal text-center">
-            <a href="https://icanhazdadjoke.com/api" rel="noopener noreferrer" target="_blank">
-              (datasource: https://icanhazdadjoke.com/api)
-            </a>
-          </small>
-        </h2>
-        <blockquote className="mx-auto text-3xl italic leading-normal text-center text-blue-200">
-          {error ? `ERROR: ${error.message}` : ''}
-          {data && !error ? (
-            <>
-              <FaQuoteLeft />
-              {` ${data.joke}`}
-            </>
-          ) : (
-            '... loading dad joke ...'
-          )}
-        </blockquote>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="">
+            <h1 className="text-5xl font-bold">
+              React/Tailwind PWA Starter Template
+            </h1>
+            <blockquote className="mx-auto text-3xl italic leading-normal text-center text-blue-200">
+              {error ? `ERROR: ${error.message}` : ''}
+              {data && !error ? (
+                <>
+                  <FaQuoteLeft />
+                  {` ${data.joke}`}
+                </>
+              ) : (
+                '... loading dad joke ...'
+              )}
+            </blockquote>
+            <h2 className="my-12 text-2xl font-semibold leading-tight text-center">
+              <small className="text-base font-normal text-center">
+                <a
+                  href="https://icanhazdadjoke.com/api"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  source: https://icanhazdadjoke.com/api
+                </a>
+              </small>
+            </h2>
+          </div>
+        </div>
       </div>
       <div className="fixed min-w-full text-base text-center bottom-2">
         <a href="https://github.com/mikesprague/react-tailwindcss-vercel-starter">
           <FaGithub />
-          {` Back to repo`}
+          {' Back to repo'}
         </a>
       </div>
     </>
